cd9eaa918660afeccfe8774d360c013abc5d1150
Hutao <75420510+Pepaya12@users.noreply.github.com>
2022-01-31 13:21:58 +0700

    feat: change name

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const NextSitemapConfig = {
-  siteUrl: "https://nextarter-chakra.sznm.dev",
+  siteUrl: "https://ioi2.vercel.app/",
   generateRobotsTxt: true,
 };
 
